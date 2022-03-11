289a0c59f1823d76cfe8d20591d3a8249bfb805e
eastcopper <ldh7228@gmail.com>
2022-03-11 23:38:43 +0900

    📑:: (#1)[Git] Git init

---
+++ src/Components/Page/Dobby/index.tsx
@@ -0,0 +1,14 @@
+import * as S from "./styles";
+
+export default function Dobby() {
+  return (
+    <>
+      <S.MainDiv>
+        <S.Title>병진이형의 입대 날짜까지..</S.Title>
+        <S.Time>
+          <div>00:00:00:00:00</div>
+        </S.Time>
+      </S.MainDiv>
+    </>
+  );
+}
---
+++ src/Components/Page/Dobby/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 200px;
+`;
+
+export const Title = styled.div`
+  color: #57a0ce;
+  font-size: 100px;
+  font-weight: bold;
+`;
+
+export const Time = styled.div`
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 1250px;
+  border-radius: 24px;
+  border: 7px solid #acf046;
+  padding-bottom: 20px;
+  div {
+    font-size: 160px;
+    color: #acf046;
+  }
+`;
