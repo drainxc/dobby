3e0aa9df76955c95758c52fc46d73a235c7673fa
eastcopper <ldh7228@gmail.com>
2022-03-12 01:19:40 +0900

    ⚡:: (#11)[Dobby] Add Time

--- src/Components/Common/Time/index.tsx
+++ src/Components/Common/Time/index.tsx
@@ -1,10 +1,34 @@
 import * as S from "./styles";
+import { useEffect, useState } from "react";
 
 export default function Time() {
+  const now = new Date();
+  const [count, setCount] = useState<number>(0);
+  const [time, setTime] = useState<timeType>({
+    year: "0",
+    month: "0",
+    day: "0",
+    hours: "0",
+    minutes: "0",
+    seconds: "0",
+  });
+
+  type timeType = {
+    year: string;
+    month: string;
+    day: string;
+    hours: string;
+    minutes: string;
+    seconds: string;
+  };
+
   return (
     <>
       <S.Time>
-        <div>00:00:00:00:00</div>
+        <div>
+          {time.year} : {time.month} : {time.day} : {time.hours} :{" "}
+          {time.minutes} : {time.seconds}
+        </div>
       </S.Time>
     </>
   );
