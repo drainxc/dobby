af21971231a44ae5dbcf93327cf54944c7d3064f
eastcopper <ldh7228@gmail.com>
2022-03-11 23:45:53 +0900

    ⚡:: (#3)[Time] Design Time

---
+++ src/Components/Common/Time/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Time = styled.div`
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 1250px;
+  border-radius: 24px;
+  border: 7px solid #acf046;
+  padding-bottom: 20px;
+  div {
+    font-size: 160px;
+    color: #acf046;
+  }
+`;
